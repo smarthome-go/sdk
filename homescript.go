940bde9d5f5462f245bf414d1f67d46adaf20104
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-05-26 00:42:36 +0200

    Added method for linting Homescript code

--- homescript.go
+++ homescript.go
@@ -114,6 +114,60 @@ func (c *Connection) RunHomescript(code string, timeout time.Duration) (response
 		return HomescriptResponse{}, ErrPermissionDenied
 	}
 	return HomescriptResponse{}, fmt.Errorf("unknown response code: %s", res.Status)
+}
+
+// Lints a string of homescript code on the Smarthome-server
+// Returns a Homescript-response and an error
+// The error is meant to indicate a failure of communication, not a failure of linting
+/** Errors
+- nil
+- ErrNotInitialized
+- ErrConnFailed
+- ErrReadResponseBody
+- ErrInvalidCredentials
+- ErrPermissionDenied
+- PrepareRequest errors
+- Unknown
+*/
+func (c *Connection) LintHomescript(code string, timeout time.Duration) (response HomescriptResponse, err error) {
+	if !c.ready {
+		return HomescriptResponse{}, ErrNotInitialized
+	}
+	req, err := c.prepareRequest("/api/homescript/lint/live", Post, struct {
+		Code string `json:"code"`
+	}{
+		Code: code,
+	})
+	if err != nil {
+		return HomescriptResponse{}, err
+	}
+	client := &http.Client{
+		Timeout: timeout,
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return HomescriptResponse{}, ErrConnFailed
+	}
+	defer res.Body.Close()
+
+	switch res.StatusCode {
+	// Either the script has beem linted successfully or errors were detected
+	case 200, 500:
+		resBody, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return HomescriptResponse{}, ErrReadResponseBody
+		}
+		var parsedBody HomescriptResponse
+		if err := json.Unmarshal(resBody, &parsedBody); err != nil {
+			return HomescriptResponse{}, ErrReadResponseBody
+		}
+		return parsedBody, nil
+	case 401:
+		return HomescriptResponse{}, ErrInvalidCredentials
+	case 403:
+		return HomescriptResponse{}, ErrPermissionDenied
+	}
+	return HomescriptResponse{}, fmt.Errorf("unknown response code: %s", res.Status)
 
 }
 
