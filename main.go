3ce8348308c637fd55ca18b519057b727bab7824
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-06-21 19:38:40 +0200

    Added comments and Smarthome version to `main.go`

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
 package sdk
 
-const Version = "0.1.0"
+// Specifies the SDK version
+const Version = "0.12.1"
+
+// Specifies the oldest Smarthome version this SDK can connect to
+const MinSmarthomeVersion = "0.0.45"
