13403dc650ae9342b100209766218ed96c7094fc
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-06-23 10:11:34 +0200

    Bump version to `0.14.1`

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package sdk
 
 // Specifies the SDK version
-const Version = "0.14.0"
+const Version = "0.14.1"
 
 // Specifies the oldest Smarthome version this SDK can connect to
 const MinSmarthomeVersion = "0.0.45"
